bb69c151b57e77fa4b85ff0db623957834857509
Colby Hale <28745762+colh99@users.noreply.github.com>
2024-01-13 15:18:13 -0800

    fixes

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 const port = 3000
 
 app.listen(process.env.port || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+  console.log('Web Server is listening at port ' + (process.env.port || port));
 });
