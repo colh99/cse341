dd39332784cf8d4d542b757a4bd37dd561fdc804
Colby Hale <28745762+colh99@users.noreply.github.com>
2024-01-27 14:10:20 -0800

    Adjusted url to require the db id instead of index. Added routes.rest for testing

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,12 +1,9 @@
 const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 // Get all contacts
 const getAllData = async (req, res, next) => {
-    const result = await mongodb
-        .getDb()
-        .db('cse341')
-        .collection('contacts')
-        .find();
+    const result = await mongodb.getDb().db('cse341').collection('contacts').find();
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists);
@@ -15,14 +12,11 @@ const getAllData = async (req, res, next) => {
 
 // Get a single contact
 const getSingleData = async (req, res, next) => {
-    const result = await mongodb
-        .getDb()
-        .db('cse341')
-        .collection('contacts')
-        .find();
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db('cse341').collection('contacts').find({ _id: userId });
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[req.params.id]); // selects the id
+        res.status(200).json(lists[0]); // selects the id
     });
 };
 
