80f929393d4f6a661b055580f8a81ae4a7e314c2
Colby Hale <28745762+colh99@users.noreply.github.com>
2024-01-27 14:43:05 -0800

    Configured eslint and applied prettier to files. Fixed routes.rest

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,22 +2,22 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 // Get all contacts
-const getAllData = async (req, res, next) => {
-    const result = await mongodb.getDb().db('cse341').collection('contacts').find();
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
+const getAllData = async (req, res) => {
+  const result = await mongodb.getDb().db('cse341').collection('contacts').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
 };
 
 // Get a single contact
-const getSingleData = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db('cse341').collection('contacts').find({ _id: userId });
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]); // selects the id
-    });
+const getSingleData = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('cse341').collection('contacts').find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]); // selects the id
+  });
 };
 
 module.exports = { getAllData, getSingleData };
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = require('express').Router();
 
 // Routes
-router.use('/contacts', require('./contacts'))
-
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
